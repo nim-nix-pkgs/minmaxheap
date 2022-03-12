a133b79f7fd2239ba5133997642d4b3d838c09d2
hugosenari <hugosenari@gmail.com>
2022-03-12 03:29:19 -0300

    chore: re index minmaxheap

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''MinMaxHeap'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-minmaxheap-master.flake = false;
+  inputs.src-minmaxheap-master.owner = "stefansalewski";
+  inputs.src-minmaxheap-master.ref   = "refs/heads/master";
+  inputs.src-minmaxheap-master.repo  = "minmaxheap";
+  inputs.src-minmaxheap-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-minmaxheap-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-minmaxheap-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
